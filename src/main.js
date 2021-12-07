af32a2bb3f34f2fe3e1014a96757665d840eb387
p-sthely <contact@philippesthely.com>
2021-12-07 14:13:48 +0100

    add highlight.js library

--- src/main.js
+++ src/main.js
@@ -1,18 +1,27 @@
 console.log('Message from src/main.js : hello!')
 
-var md = window.markdownit();
+var md = window.markdownit({
+    highlight: function (str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+            try {
+                return hljs.highlight(str, { language: lang }).value;
+            } catch (__) { }
+        }
+
+        return ''; // use external default escaping
+    }
+});
+
 
 function renderMarkdownFile(file) {
     var request = new XMLHttpRequest();
     request.open('GET', './src/content/' + file);
     request.responseType = 'text';
-    
-    request.onload = function() {
-        let mdContent = request.response;
-        mdContent = md.render(mdContent);
+
+    request.onload = function () {
+        let mdContent = md.render(request.response);
         document.getElementById('markdown').innerHTML = mdContent;
     };
-    
+
     request.send();
 }
-
