3867f77988277bae6827b7ce9eeac7a2d925723d
p-sthely <contact@philippesthely.com>
2021-12-07 14:22:48 +0100

    reformat code

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-console.log('Message from src/main.js : hello!')
+console.log('Message from src/main.js : hello!');
 
 var md = window.markdownit({
-    highlight: function (str, lang) {
-        if (lang && hljs.getLanguage(lang)) {
+    highlight: function ( str, lang ) {
+        if ( lang && hljs.getLanguage( lang ) ) {
             try {
-                return hljs.highlight(str, { language: lang }).value;
-            } catch (__) { }
+                return hljs.highlight( str, { language: lang } ).value;
+            } catch ( __ ) { }
         }
 
         return ''; // use external default escaping
@@ -13,14 +13,14 @@ var md = window.markdownit({
 });
 
 
-function renderMarkdownFile(file) {
+function renderMarkdownFile( file ) {
     var request = new XMLHttpRequest();
-    request.open('GET', './src/content/' + file);
+    request.open( 'GET', './src/content/' + file );
     request.responseType = 'text';
 
     request.onload = function () {
-        let mdContent = md.render(request.response);
-        document.getElementById('markdown').innerHTML = mdContent;
+        let mdContent = md.render( request.response );
+        document.getElementById( 'markdown' ).innerHTML = mdContent;
     };
 
     request.send();
