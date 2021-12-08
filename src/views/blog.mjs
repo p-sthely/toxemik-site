0d1a6f66340b8092bdbd36af05954e194584f402
p-sthely <contact@philippesthely.com>
2021-12-08 10:59:17 +0100

    add some style to the modal

--- src/views/blog.mjs
+++ src/views/blog.mjs
@@ -20,14 +20,13 @@ const template = `
         <main>
             <div class="modal" id="markdownModal" tabindex="-1" aria-labelledby="markdownModalLabel" aria-hidden="true">
                 <div class="modal-dialog m-0">
-                    <div class="modal-content">
+                    <div class="modal-content rounded-0 border-0">
                         <div class="modal-header sticky-top bg-light">
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div id="markdown" class="modal-body"></div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
                         </div>
                     </div>
                 </div>
